d515613f090ebf0611ed0be9f15c2fa5a5d9e557
Albdroid.AL <sxt@usa.com>
2022-06-09 23:35:40 +0200

    Update test.php

--- test.php
+++ test.php
@@ -6,6 +6,7 @@ $api = new \Vudeo\Streaming\Api($client);
 // Your own api key from https://vudeo.io/?op=my_account
 $api->set_api_key("ENTER_API_KEY");
 
+// full url https://vudeo.io/aqgypxct9pmq.html
 $direct_link = $api->get_direct_link("aqgypxct9pmq"); // file id
 //echo json_encode($direct_link);
 
@@ -22,13 +23,15 @@ $account_stats_last = $api->get_account_stats_last("7");
 VIDEO UPLOADER
 URL TO UPLOADING
 */
-$upload = $api->upload("VIDEO_URL_TO_UPLOADING");
+// example url https://my_server.io/video.mp4
+$upload = $api->upload("VIDEO_URL_TO_UPLOADING_HERE");
 //echo json_encode($upload);
 
 // GET UPLOADING SERVER
 $upload_server = $api->get_uploading_server();
 //echo json_encode($upload_server);
 
+// full url https://vudeo.io/aqgypxct9pmq.html
 $get_file_info = $api->get_file_info("aqgypxct9pmq"); // file id
 //echo json_encode($get_file_info);
 
@@ -37,5 +40,7 @@ $get_file_list = $api->get_file_list();
 //echo json_encode($get_file_list);
 
 // Group Folders
+// example https://vudeo.io/?op=my_files&fld_id=772
 $get_folder_list = $api->get_folder_list("772"); // folder id
 //echo json_encode($get_folder_list);
+?>
